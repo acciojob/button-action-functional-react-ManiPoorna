fad311a0a42bf082125379105953ddb4d12c66be
Mani Poorna <ndmprao2001@gmail.com>
2023-10-21 12:33:45 +0530

    V 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
+import { render } from "enzyme";
 
 function App() {
+  const [render,setRender] = useState(false)
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button id="click" onClick={()=>setRender(true)}>CLick</button>
+      {
+        render ? <p id="para">Hello, I've learnt to use the full-stack evaluation tool.
+          This makes me so happy</p>
+          : ""
+      }
     </div>
   );
 }
